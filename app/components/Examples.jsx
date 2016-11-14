33f0a7c3aa224ccce898676e7a5bf852a9559222
Netanel Hury <netanel@soluto.com>
2016-11-14 09:12:41 +0200

    Add Grid to Examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,18 @@
 const React = require('react');
+const {Link} = require('react-router');
 
-const Examples = () => <div><h3>Examples</h3><p>Welcome to examples page!</p></div>;
+const Examples = () =>
+    <div>
+        <h1>Examples</h1>
+        <p>Here are a few example locations to try out:</p>
+        <ol>
+            <li>
+                <Link to="/?location=Philadelphia">Philadelphia, PA</Link>
+            </li>
+            <li>
+                <Link to="/?location=Rio">Rio, Brazil</Link>
+            </li>
+        </ol>
+    </div>;
 
 module.exports = Examples;
